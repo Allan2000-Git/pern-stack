76913d84531c95cc88dfc2464b45debdc259bb74
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-02-11 16:34:52 +0530

    connect to postgres db

--- server/routes/restaurants.js
+++ server/routes/restaurants.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../db/index')
 
-router.get("/api/v1/restaurants", (req, res) => {
-    res.status(200).json({
-        data:{
-            restaurants:["test1", "test2", "test3"]
-        }
-    })
+router.get("/api/v1/restaurants", async (req, res) => {
+    // const {rows} = await db.query('SELECT * FROM restaurants');
+    res.status(200).json(rows);
 })
 
-router.get("/api/v1/restaurants/:id", (req, res) => {
+router.get("/api/v1/restaurants/:id", async (req, res) => {
+    // const {rows} = await db.query('SELECT * FROM restaurants');
     res.status(200).json(req.params.id)
 })
 
 router.post("/api/v1/restaurants/", (req, res) => {
+    res.status(201).json(req.body)
+})
+
+router.patch("/api/v1/restaurants/:id", (req, res) => {
     res.status(200).json(req.body)
+})
+
+router.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(200).json(req.params.id)
 })
 
 module.exports = router;
