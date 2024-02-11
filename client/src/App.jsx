86aebd3ff98c57e8e13b89d1104c2547b56b3dd7
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-02-11 23:06:30 +0530

    add pages, routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from './pages/Home'
+import RestaurantDetail from './pages/RestaurantDetail'
+import RestaurantEdit from './pages/RestaurantEdit'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        Hello World
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/restaurants/:id" element={<RestaurantDetail/>} />
+          <Route path="/restaurants/:id/edit" element={<RestaurantEdit/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
