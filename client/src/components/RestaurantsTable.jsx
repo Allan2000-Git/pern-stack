0dbc33038c71f920da951e3aec2a295ca809a1b4
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-02-14 23:50:01 +0530

    fetch the restaurants data and store in context api

--- client/src/components/RestaurantsTable.jsx
+++ client/src/components/RestaurantsTable.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import yelpApi from '../api/yelpApi';
+import { useYelpContext } from '../context/YelpContext';
 
 const RestaurantsTable = () => {
+    const {restaurants, setRestaurants} = useYelpContext();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await yelpApi.get("/");
+                setRestaurants(response.data)
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+
+        fetchData();
+    },[])
+
     return (
         <>
             <table className="table table-dark table-striped table-hover mt-5">
@@ -14,7 +31,7 @@ const RestaurantsTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
+                    <tr className="h-25">
                         <td>Mc Donalds</td>
                         <td>Mangalore</td>
                         <td>5</td>
